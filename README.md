ab0aec2382b54b2940fd12ef5a6b28eac6bc1355
Bloodsplash <abhinavchaudhary1318@gmail.com>
2023-12-18 17:46:05 +0530

    readme

--- README.md
+++ README.md
@@ -1,8 +1,80 @@
-# React + Vite
+# HomePage
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+in App.jsx, it contains all the sections of homepage
 
-Currently, two official plugins are available:
+## Sections
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Navbar
+- Hero Section
+- Popular Categories Section
+- Who we are Section 
+- Feedback Section
+- Home Banner Section
+- How we work Section
+- Offices
+- Footer
+
+## Components
+
+All the components of Homepage, for other pages code in specific file
+
+# About Page
+
+All code provided in pages folder -> About.jsx, css in classes
+
+# Contact Page
+
+All code provided in pages folder -> Contact.jsx, css in classes
+
+# Product Caregories Page
+
+All code provided in pages folder -> ProdCategories, css in classes
+
+# Main.jsx 
+
+contains routes
+
+# Index.css
+Major stylings
+
+
+# data.js
+
+contains all the data of feedback, clients, our promises, etc
+
+
+# Tailwind.config.js
+
+contains major style configs like fonts, sizing, colors
+
+colors: {
+        primary: '#f3bb18', yellow color of site
+        secondary: '#f6f6f6', dull white color of site
+        dark: '#1c1a18', black color of site
+      },
+fontFamily: {
+clash: ['Clash Display', 'sans-serif'],
+bebas: ['Bebas Neue', 'sans-serif'],
+poppins: ['Poppins', 'sans-serif'],
+grotesk: ['Clash Grotesk', 'sans-serif'],
+outfit: ['Outfit', 'Arial', 'sans-serif'],
+newsreader: ['Newsreader', 'serif'], font used in site as font-newsreader
+// ...other font variables
+},
+fontSize: {
+'xs': '0.75rem',
+'sm': '0.875rem',
+'base': '1rem',
+'lg': '1.125rem',
+'xl': '1.25rem',
+'2xl': '1.5rem',
+'3xl': '1.875rem',
+'4xl': '2rem',
+'5xl': '3rem',
+'6xl': '4rem', text-6xl is this font
+'marquee': '7rem',
+'head': '8rem',
+'bigmarquee': '15rem',
+'litmarquee': '1rem',
+}
+
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -59,7 +59,7 @@
 //                 <hr />
 //               </div>
 //               </div>
-              
+
 //             </div>
 //           </div>
 //         </div>
@@ -108,111 +108,169 @@
 
 // export default About
 
-
-import React from 'react'
-import Navbar from '../components/Navbar'
-import MyMarquee from '../components/MyMarquee'
-import Feedback from '../components/Feedback'
-import FeedbackCards from '../components/FeedbackCards'
-import { promise } from '../components/data'
+import React from "react";
+import Navbar from "../components/Navbar";
+import MyMarquee from "../components/MyMarquee";
+import Feedback from "../components/Feedback";
+import FeedbackCards from "../components/FeedbackCards";
+import { promise } from "../components/data";
 import { PiHandshake } from "react-icons/pi";
-import { DarkButton, LightButton } from '../components/Button'
-import Footer from '../components/Footer'
+import { DarkButton, LightButton } from "../components/Button";
+import Footer from "../components/Footer";
 
 const About = () => {
   return (
     <div>
-      <div className='relative h-screen'>
+      <div className="relative h-screen">
         <Navbar />
-        <img src='https://ik.imagekit.io/hfrhxebxv/Sunchem%20Assets/nick-night-52F2gX0COs4-unsplash.jpg?updatedAt=1702805986125' className='w-full h-full absolute top-0 -z-10 object-cover' alt='...' />
-        <h2 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl mt-8 md:mt-16 text-secondary w-11/12 mx-auto top-1/2 absolute left-8'>
-          <span className='italic'>Beauty</span> from <br /> skin to <span className='italic'>soul</span>
+        <img
+          src="https://ik.imagekit.io/hfrhxebxv/Sunchem%20Assets/nick-night-52F2gX0COs4-unsplash.jpg?updatedAt=1702805986125"
+          className="w-full h-full absolute top-0 -z-10 object-cover"
+          alt="..."
+        />
+        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl mt-8 md:mt-16 text-secondary w-11/12 mx-auto top-1/2 absolute left-8">
+          <span className="italic">Beauty</span> from <br /> skin to{" "}
+          <span className="italic">soul</span>
         </h2>
       </div>
-      <div className='w-11/12 mx-auto relative mt-8 md:mt-16'>
-        <h2 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl'>Sun Chem India <br /><span className='italic'>since</span> 1985</h2>
-        <div className='flex flex-col md:flex-row justify-between gap-8 items-start py-8'>
-          <div className='w-full md:w-1/3'>
-            <h2 className='italic text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl'>Our Company</h2>
-            <p>Sunchem India is a manufacturer of high quality skincare and homecare products. We are involved in manufacturing formulations, providing our customers with market specific solutions packaging and distribution as well as worldwide customer support.</p>
+      <div className="w-11/12 mx-auto relative mt-8 md:mt-16">
+        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl">
+          Sun Chem India <br />
+          <span className="italic">since</span> 1985
+        </h2>
+        <div className="flex flex-col md:flex-row justify-between gap-8 items-start py-8">
+          <div className="w-full md:w-1/3">
+            <h2 className="italic text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl">
+              Our Company
+            </h2>
+            <p>
+              Sunchem India is a manufacturer of high quality skincare and
+              homecare products. We are involved in manufacturing formulations,
+              providing our customers with market specific solutions packaging
+              and distribution as well as worldwide customer support.
+            </p>
           </div>
-          <div className='w-full md:w-2/3'>
-            <h2 className='italic text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl'>Professional Skills & Great Equipments</h2>
-            <p>We proudly guarantee the quality of our products based on the professional knowledge and know-how of our senior researchers with 35+ years of experience and latest sophisticated manufacturing equipment. High tech machinery and amongst india’s most advanced state of the art research centre our team is the best in the industry. We are not only your research partner but a strategic business partner.</p>
+          <div className="w-full md:w-2/3">
+            <h2 className="italic text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl">
+              Professional Skills & Great Equipments
+            </h2>
+            <p>
+              We proudly guarantee the quality of our products based on the
+              professional knowledge and know-how of our senior researchers with
+              35+ years of experience and latest sophisticated manufacturing
+              equipment. High tech machinery and amongst india’s most advanced
+              state of the art research centre our team is the best in the
+              industry. We are not only your research partner but a strategic
+              business partner.
+            </p>
           </div>
         </div>
       </div>
       <MyMarquee />
-      <div className='my-8 md:my-16'>
-        <img src='https://ik.imagekit.io/hfrhxebxv/Sunchem%20Assets/pexels-sora-shimazaki-5938245.jpg?updatedAt=1702700240179' className='w-full h-[50vh] md:h-[80vh] object-cover' alt='...' />
-        <div className='flex flex-col md:flex-row gap-8 w-11/12 mx-auto my-8 md:my-16'>
-          <div className='w-full md:w-2/3'>
-            <img src='https://ik.imagekit.io/hfrhxebxv/Sunchem%20Assets/sample_products.jpg?updatedAt=1702699682206' className='rounded-2xl h-[50vh] md:h-[75vh] w-full object-cover' alt='' />
+      <div className="my-8 md:my-16">
+        <img
+          src="https://ik.imagekit.io/hfrhxebxv/Sunchem%20Assets/pexels-sora-shimazaki-5938245.jpg?updatedAt=1702700240179"
+          className="w-full h-[50vh] md:h-[80vh] object-cover"
+          alt="..."
+        />
+        <div className="flex flex-col md:flex-row gap-8 w-11/12 mx-auto my-8 md:my-16">
+          <div className="w-full md:w-2/3">
+            <img
+              src="https://ik.imagekit.io/hfrhxebxv/Sunchem%20Assets/sample_products.jpg?updatedAt=1702699682206"
+              className="rounded-2xl h-[50vh] md:h-[75vh] w-full object-cover"
+              alt=""
+            />
           </div>
-          <div className='w-full md:w-1/3'>
-          <p>Sunchem India is a manufacturer of high quality skincare and homecare products. We are involved in manufacturing formulations, providing our customers with market specific solutions packaging and distribution as well as worldwide customer support. We ensure best service with every product we have manufactured.</p>
-<div className='grid grid-cols-2 gap-4'>
-<div className='flex justify-evenly p-4 flex-col gap-4'>
-  <h2 className='font-semibold font-poppins text-4xl'>166+</h2>
-  <p>Our current team count, up from 3 in 2016</p>
-  <hr />
-</div>
-<div className='flex justify-evenly p-4 flex-col gap-4'>
-  <h2 className='font-semibold font-poppins text-4xl'>166+</h2>
-  <p>Our current team count, up from 3 in 2016</p>
-  <hr />
-</div>
-<div className='flex justify-evenly p-4 flex-col gap-4'>
-  <h2 className='font-semibold font-poppins text-4xl'>166+</h2>
-  <p>Our current team count, up from 3 in 2016</p>
-  <hr />
-</div>
-<div className='flex justify-evenly p-4 flex-col gap-4'>
-  <h2 className='font-semibold font-poppins text-4xl'>166+</h2>
-  <p>Our current team count, up from 3 in 2016</p>
-  <hr />
-</div>
-</div>
-              
+          <div className="w-full md:w-1/3">
+            <p>
+              Sunchem India is a manufacturer of high quality skincare and
+              homecare products. We are involved in manufacturing formulations,
+              providing our customers with market specific solutions packaging
+              and distribution as well as worldwide customer support. We ensure
+              best service with every product we have manufactured.
+            </p>
+            <div className="grid grid-cols-2 gap-4">
+              <div className="flex justify-evenly p-4 flex-col gap-4">
+                <h2 className="font-semibold font-poppins text-4xl">166+</h2>
+                <p>Our current team count, up from 3 in 2016</p>
+                <hr />
+              </div>
+              <div className="flex justify-evenly p-4 flex-col gap-4">
+                <h2 className="font-semibold font-poppins text-4xl">166+</h2>
+                <p>Our current team count, up from 3 in 2016</p>
+                <hr />
+              </div>
+              <div className="flex justify-evenly p-4 flex-col gap-4">
+                <h2 className="font-semibold font-poppins text-4xl">166+</h2>
+                <p>Our current team count, up from 3 in 2016</p>
+                <hr />
+              </div>
+              <div className="flex justify-evenly p-4 flex-col gap-4">
+                <h2 className="font-semibold font-poppins text-4xl">166+</h2>
+                <p>Our current team count, up from 3 in 2016</p>
+                <hr />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div className='w-11/12 mx-auto'>
+      <div className="w-11/12 mx-auto">
         <Feedback start={`In the`} end={``} italic={`Spotlight`} />
       </div>
-      <div className='w-11/12 mt-16 mx-auto relative'>
-        <h2 className='text-3xl md:text-4xl lg:text-5xl pb-8 md:pb-16 text-center'>Our <span className="italic">Promise</span></h2>
-        <div className='flex flex-wrap justify-center gap-8'>
+      <div className="w-11/12 mt-16 mx-auto relative">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl pb-8 md:pb-16 text-center">
+          Our <span className="italic">Promise</span>
+        </h2>
+        <div className="flex flex-wrap justify-center gap-8">
           {promise.map((item, index) => (
-            <div key={index} className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 rounded-xl text-center h-auto bg-secondary p-4 bg-opacity-20  backdrop-blur-md bg-clip-padding backdrop-filter border border-opacity-30 border-white shadow-md mb-8 md:mb-0'>
-              <div className='flex justify-center'><PiHandshake className='my-4 text-center' size={45} /></div>
-              <h2 className='py-2'>{item.title}</h2>
-              <p className='py-2'>{item.description}</p>
+            <div
+              key={index}
+              className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 rounded-xl text-center h-auto bg-secondary p-4 bg-opacity-20  backdrop-blur-md bg-clip-padding backdrop-filter border border-opacity-30 border-white shadow-md mb-8 md:mb-0"
+            >
+              <div className="flex justify-center">
+                <PiHandshake className="my-4 text-center" size={45} />
+              </div>
+              <h2 className="py-2">{item.title}</h2>
+              <p className="py-2">{item.description}</p>
             </div>
           ))}
         </div>
-        <img src='https://ik.imagekit.io/hfrhxebxv/Sunchem%20Assets/ellipse.png?updatedAt=1702548819472' className='absolute top-1/3 left-1/3 -z-10' alt='...' />
+        <img
+          src="https://ik.imagekit.io/hfrhxebxv/Sunchem%20Assets/ellipse.png?updatedAt=1702548819472"
+          className="absolute top-1/3 left-1/3 -z-10"
+          alt="..."
+        />
       </div>
-      <div className='w-11/12 mx-auto py-8 md:py-16'>
-        <h2 className='text-3xl md:text-4xl lg:text-5xl text-center pb-8 md:pb-16'><span className='italic'>Reach</span> Us Out</h2>
-        <div className='flex flex-col md:flex-row gap-4'>
-          <div className='bg-secondary rounded-3xl p-8 w-full md:w-1/2 h-[50vh] lg:h-[70vh] flex flex-col justify-evenly gap-4'>
-            <h2 className='text-4xl md:text-5xl lg:text-6xl xl:text-7xl'><span className='italic'>Connect</span> with us</h2>
-            <p>Ipsum is simply dummy text of the printing and typesetting industry...</p>
-            <p className='font-semibold'>Let’s start your brand’s journey!</p>
-            <div className='flex gap-4'>
+      <div className="w-11/12 mx-auto py-8 md:py-16">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl text-center pb-8 md:pb-16">
+          <span className="italic">Reach</span> Us Out
+        </h2>
+        <div className="flex flex-col md:flex-row gap-4">
+          <div className="bg-secondary rounded-3xl p-8 w-full md:w-1/2 h-[50vh] lg:h-[70vh] flex flex-col justify-evenly gap-4">
+            <h2 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
+              <span className="italic">Connect</span> with us
+            </h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil a assumenda dolores aliquid quos, nam illo ratione ullam. Fugiat, quasi?
+            </p>
+            <p className="font-semibold">Let’s start your brand’s journey!</p>
+            <div className="flex gap-4">
               <DarkButton name={`Know More`} />
               <LightButton name={`Get in Touch`} />
             </div>
           </div>
-          <div className='w-full md:w-1/2 h-[50vh] lg:h-[70vh]'>
-            <img src='https://ik.imagekit.io/hfrhxebxv/Sunchem%20Assets/pexels-fox-1595385.jpg?updatedAt=1702470066225' className='rounded-3xl w-full h-full object-cover' alt='...' />
+          <div className="w-full md:w-1/2 h-[50vh] lg:h-[70vh]">
+            <img
+              src="https://ik.imagekit.io/hfrhxebxv/Sunchem%20Assets/pexels-fox-1595385.jpg?updatedAt=1702470066225"
+              className="rounded-3xl w-full h-full object-cover"
+              alt="..."
+            />
           </div>
         </div>
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default About;
